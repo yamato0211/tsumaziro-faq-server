a975c1b1e733ebf986189edd10493ecf7abb1de6
yamato0211 <kumayama0211@gmail.com>
2024-02-17 20:02:12 +0900

    add: migration

--- migration/migration.go
+++ migration/migration.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	"github.com/yamato0211/tsumaziro-faq-server/db/model"
 	"github.com/yamato0211/tsumaziro-faq-server/pkg/config"
 	"github.com/yamato0211/tsumaziro-faq-server/pkg/db"
@@ -8,6 +10,9 @@ import (
 
 func main() {
 	d := db.NewMySQLConnector(config.NewDBConfig())
+	if _, err := d.DB.NewDropTable().Model(&model.Account{}).Exec(context.TODO()); err != nil {
+		panic(err)
+	}
 	if err := model.Migrate(d); err != nil {
 		panic(err)
 	}
